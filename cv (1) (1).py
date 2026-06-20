7f56851325cbf0af5eda812bb0447836e2700b49
smamoudou776-code <smamoudou776@gmail.com>
2026-06-20 14:05:05 +0100

    Add files via upload

---
+++ cv (1) (1).py
@@ -0,0 +1,71 @@
+import streamlit as cv
+# configuration de la papge
+cv .set_page_config(page_title="cv - CHEIKH IBRA NDIAYE", page_icon="photo.pjg",layout= "centered")#
+# titre
+cv.title("CHEIKH IBRA NDIAYE")
+cv. write ("Ingénieur en  Aménagemnagement et gestionnaire urbaine en dote dune compétence de planification et aménagement urbaine du territoire  .Expert, Téchnicien superieur  en géomatique avec la maitrise des outils dinformation géographie (SIG),cartographie,analyse des données spatial ,dessin architecture ( plan dhabitation ,commercial) et programmation, base donnée.Télédection comtrole drone,Topographe. Je suis certifié sur en collect de données quantitative au niveau de lANSD en plus jai aqui les compétences en labaratoir géotechnique  ")
+
+cv.title("Parcour")
+
+cv.write("Master 2 en Aménagement et Gestion Urbaine en Afrique à ľuniversité Cheikh Anta Diop de Dakar")
+cv. write ("Université  Cheikh Anta Diop de Dakar ")
+cv.write ("Année:2025/2026")
+cv.write(" Premier Année-BTS Geomatique  ")
+cv. write ("Centrte dentrepreneuriat et de Developpement Technique ")
+cv.write ("Annee 2025/2026")
+cv.write("Licence en GouvernanceTerritoriale et Developpement Local Option en Aménagement du Territoire et Développement Local ")
+cv. write ("Universite  Cheikh Anta Diop de Dakar (UCAD) / Institut de la Gouvernance Territorial et Développement Local (IGT)")
+cv.write ("Année 2025/2026")
+cv.write("Baccalauriat en Science Humaine et Social ")
+cv. write ("Lycee Mame Thierno Birahim Mbacke")
+cv.write ("Année:2020")
+
+cv.title ("Certification")
+
+cv.write(" Attestation en Laboratoir Geotechnique ")
+cv. write ("CFPT-Castor")
+cv.write("Attestation en Technologie Immersive VR/AR/MR ")
+cv. write ("CURI")
+
+cv. write ("Attestation en Infographie")
+cv.write ("Skill Up")
+cv. write (" Attestation en Prestation de Service Rnu")
+cv.write ("ANSD ")
+cv. write (" Attestation en Informatique et Internet ")
+cv.write ("FORCE-N")
+cv. write ("Attestation en Intelligence le Pour Tous")
+cv.write (" FORCE-N ")
+cv. write (" Attestation en Traitement de Donnee")
+cv.write (" FORCE-N ")
+cv.write ("Année 2024")
+cv.write (" Attestation du Projet de Formation sur la Valorisation des Ressource et Innovation Expoitation Economique du Senegal PFVRIEES") 
+cv.write ("UNCHK")
+
+cv.title ("COMPETENCES")
+cv.write(" Microsofte office")
+cv.write("Cartograp Topographie ")
+cv.write("Planification")
+cv.write("Déssin architect")
+cv.write("Collecte données") 
+cv.write("Base de données spatiales") 
+cv.write("Aménagement du Territoire")
+cv.write("Planification du Territoire")
+cv.write("Diagnostique Territorial")
+cv.write ("Essaie Laboratoire Gétechnique")
+cv.write("Carte visite,invitation,flyers")
+cv.write("Numerisation") 
+cv.write("Anacondant,Python,Jupterlab streamlit")
+with cv.sidebar:
+    cv.title("CHEIKH IBRA NDIAYE")             
+    cv.write( "Ingénieur Aménagiste/Technicien en Géomatique")  
+    cv.write("Email:wwwcheikhibrandiaye@gmail.com")
+    cv.write ("Téléphone:784793519")
+    cv.write("Adresse:Dakar/Sénégal")
+    cv.write( "Situation Matrimonial:Célibataire sans enfant")
+
+
+
+
+
+
+
